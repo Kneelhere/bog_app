41a62ab0abb00f3551f370a6f22fcf933052ff6c
Neil Rayos <Kneelhere@Neils-MacBook-Pro.local>
2015-09-20 15:44:16 -0700

    added a new route for a new form

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'creatures#index'
-  get '/creatures', to: "creatures#index" 
+  get '/creatures', to: "creatures#index"
+  get '/creatures/new', to: 'creatures#new'
 end
