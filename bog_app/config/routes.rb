8bdf150009e4184f0450cffd0442b237721e9d51
Neil Rayos <Kneelhere@Neils-MacBook-Pro.local>
2015-09-20 19:29:50 -0700

    added a get creatures id route to be able to view later on

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'creatures#index'
   get '/creatures', to: "creatures#index"
   get '/creatures/new', to: 'creatures#new'
+
+  get 'creatures/:id', to: 'creatures#create'
   post '/creatures', to: 'creatures#create'
 end
